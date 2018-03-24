4c1fa46547a5ac0c71ae16bb0e58c9c7de399d6c
Fredriclam <fredriclam@live.com>
2018-03-24 02:36:45 -0400

    makemap.js and spawnNote.js implemented, tested in main.html

--- static/makemap.js
+++ static/makemap.js
@@ -1,30 +1,37 @@
+function setupMap() {
+  var passageTexture = PIXI.Texture.fromImage('static/images/placeholder_passage.png')
+  var wallTexture = PIXI.Texture.fromImage('static/images/placeholder_wall.png') //("{{url_for('static', filename='images/placeholder_wall.png')}}")
+  return {passageTexture:passageTexture, wallTexture:wallTexture}
+}
+
 // Load tile textures
-var passageTexture = PIXI.Texture.fromImage('static/images/placeholder_passage.png')
-var wallTexture = PIXI.Texture.fromImage('static/images/placeholder_wall.png') //("{{url_for('static', filename='images/placeholder_wall.png')}}")
-// Create map container that contains tiles
-var overworldMapContainer = new PIXI.Container();
-// Add container to stage
-app.stage.addChild(overworldMapContainer);
-// Generate map sample data
-mapData = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0,],[0, 1 ,1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-var tileWidth = 32;
-var tileHeight = 32;
-// Popular and render tiles
-for (var y = 0; y < mapData.length; y++) {
-  for (var x = 0; x < mapData[y].length; x++){
-    // Assign graphic to terrain type
-    switch (mapData[y][x]){
-      case 0:
-        var tile = new PIXI.Sprite(wallTexture);
-        break;
-      case 1:
-        var tile = new PIXI.Sprite(passageTexture);
-        break;
+function buildMap(mapTextures){
+  // Create map container that contains tiles
+  var overworldMapContainer = new PIXI.Container();
+  // Add container to stage
+  app.stage.addChild(overworldMapContainer);
+  // Generate map sample data
+  mapData = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0,],[0, 1 ,1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+  var tileWidth = 32;
+  var tileHeight = 32;
+  // Popular and render tiles
+  for (var y = 0; y < mapData.length; y++) {
+    for (var x = 0; x < mapData[y].length; x++){
+      // Assign graphic to terrain type
+      switch (mapData[y][x]){
+        case 0:
+          var tile = new PIXI.Sprite(mapTextures.wallTexture);
+          break;
+        case 1:
+          var tile = new PIXI.Sprite(mapTextures.passageTexture);
+          break;
+      }
+      // Set anchor to top left corner
+      tile.anchor.set(0.0);
+      tile.x = x * tileWidth;
+      tile.y = y * tileHeight;
+      overworldMapContainer.addChild(tile);
     }
-    // Set anchor to top left corner
-    tile.anchor.set(0.0);
-    tile.x = x * tileWidth;
-    tile.y = y * tileHeight;
-    overworldMapContainer.addChild(tile);
   }
+  return overworldMapContainer;
 }
